59659bcb6c10c852a9c1f63d4b9e09ec39367be9
W3AXL <W3AXL@users.noreply.github.com>
2021-02-21 23:21:26 -0600

    added basic csq functionality
    added an optional parameter to the config json file
    
    per channel, you can specify a "csqLeveldBFS" parameter which is the squelch threshold in dBFS (the same unit SDR sharp uses). A value of 0, or the parameter missing entirely, will result in open squelch.

--- multifm/fm_demod.h
+++ multifm/fm_demod.h
@@ -2,6 +2,15 @@
 
 #include <tsl/result.h>
 
+/**
+ *  divisor to convert raw sample power to microwatts for dBm calculation,
+ *  found experimentally using a known channel power measured via SDRSharp.
+ *  There's probably a much better way to calculated dBFS but this works "well enough," 
+ *  and dbFS is a pretty relative measurement to begin with.
+ */
+#define SMP_SCALE   2300.0
+#define SMP_OFFSET  -4.1
+
 struct demod_base;
 
 /**
@@ -26,7 +35,7 @@ aresult_t multifm_fm_demod_init(struct demod_base **pdemod);
  * out to the real-valued PCM buffer.
  */
 aresult_t multifm_fm_demod_process(struct demod_base *demod, int16_t *in_samples, size_t nr_in_samples,
-        int16_t *out_samples, size_t *pnr_out_samples, size_t *pnr_out_bytes);
+        int16_t *out_samples, size_t *pnr_out_samples, size_t *pnr_out_bytes, int csq_level_dbfs);
 
 /**
  * Cleanup the resources used by the FM demodulator
